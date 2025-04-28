cd41c7db6ead44adb0b62a2269e657ab2cc926ba
Farhan Tanvir Khan <90358727+ftkovr9k@users.noreply.github.com>
2025-04-29 01:10:23 +0600

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name="caesar-cli",
-    version="0.1",
+    name="caesar",
+    version="0.1.4",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -10,13 +13,12 @@ setup(
         ],
     },
     install_requires=[],
-    python_requires=">=3.13",
     author="Farhan Khan",
     author_email="ftkover9k@gmail.com",
     description="A Caesar Cipher encryption and decryption program",
-    long_description=open('README.md', encoding='utf-8').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ftkovr9k/caesar-cli",
+    url="https://github.com/ftkovr9k/Caesar-Encryption-Program",
     classifiers=[
         "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: MIT License",
